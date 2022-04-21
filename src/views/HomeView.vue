dc4d96fbb7d12ea8f57f1176806344895e1c1b24
Miguel Briseño <miguel.briseno.bustos@gmail.com>
2022-04-21 12:45:41 -0500

    Listo el front

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,28 @@
 <template>
   <v-container>
-    Hola
+    <br><br>
+    <v-row justify="center">
+      <v-col cols="6">
+        <v-row justify="center">
+          <v-col cols="10">
+            <v-text-field label="Nueva tarea..." outlined></v-text-field>
+          </v-col>
+          <v-col cols="10">
+            <v-btn block class="primary" large>Agregar <v-icon right dark>mdi-plus-thick</v-icon></v-btn>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col cols="6">
+        <v-card>
+          <h2 class="text-center">LISTA DE TAREAS PENDIENTES</h2>
+          <br>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
-
   export default {
     
   }
